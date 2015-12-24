de498e6ed4587c0aaa1cfd8f238655da735faf15
Chris Cantu <chris.cantu@allclearid.com>
2015-12-23 20:24:07 -0600

    Added .otto directory to gitignore

--- app/daos/database.js
+++ app/daos/database.js
@@ -1,4 +1,4 @@
-var db = require('rethinkdbdash')({host: 'rethinkdb', db: 'teamsync'});
+var db = require('rethinkdbdash')({host: 'rethinkdb.service.consul', db: 'teamsync'});
 
 exports.Table = function Table(tableName) {
     var table = db.table(tableName);
